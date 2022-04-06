956bbd3e248e97e37cb4980466eec45f155b2ec5
Serhii Hladkov <serhii.hladkov@p2h.com>
2022-04-06 21:24:46 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  runtimeCompiler: true,
   lintOnSave: true,
   css: {
     sourceMap: true,
